4f586d34a592fea497368d9919cba3e7e1be5ddf
0xThresh.eth <0xthresh@protonmail.com>
2024-02-07 14:35:33 -0800

    Got further into the node build process

--- node-setup.sh
+++ node-setup.sh
@@ -54,5 +54,64 @@ latestFile=$(cat latest.txt)
 wget -c "https://pocket-snapshot.liquify.com/files/$latestFile" -O - | tar -xv -C /mnt/data/.pocket/data
 rm latest.txt
 
+# TODO: Enter account creation steps 
+
+# Take a backup of the existing config file
+cp /mnt/data/.pocket/config/config.json /mnt/data/.pocket/config/config.json.bak 
+
+# Insert seeds into config
+export SEEDS=$(curl -s https://raw.githubusercontent.com/pokt-network/pocket-seeds/main/mainnet.txt \
+| tr '\n' ',' \
+| sed 's/,*$//')
+pocket util print-configs \
+| jq --arg seeds "$SEEDS" '.tendermint_config.P2P.Seeds = $seeds' \
+| jq '.pocket_config.rpc_timeout = 15000' \
+| jq '.pocket_config.rpc_port = "8082"' \
+| jq '.pocket_config.remote_cli_url = "http://localhost:8082"' \
+| jq . > ~/.pocket/config/config.json
+
+# Create chains.json using default settings
+# TODO: Use 'pocket util', not doing that for now since it requires user input 
+cat << EOF > /mnt/data/.pocket/config/chains.json
+[
+  {
+    "id": "0001",
+    "url": "http://127.0.0.1:8082/",
+    "basic_auth": {
+      "username": "",
+      "password": ""
+    }
+  }
+]
+EOF
+
+# Create genesis.json
+cd /mnt/data/.pocket/config
+wget https://raw.githubusercontent.com/pokt-network/pocket-network-genesis/master/mainnet/genesis.json genesis.json
+
+# Increase pocket user's ulimit 
+echo "pocket soft nofile 16384" >> /etc/security/limits.conf
+
+cat << EOF > /etc/systemd/system/pocket.service
+[Unit]
+Description=Pocket Service
+After=network.target mnt-data.mount
+Wants=network-online.target systemd-networkd-wait-online.service
+
+[Service]
+User=pocket
+Group=sudo
+ExecStart=/mnt/data/go/bin/pocket start
+ExecStop=/mnt/data/go/bin/pocket stop
+
+[Install]
+WantedBy=default.target
+EOF
+
 # Make sure all files are owned by the Pocket user before continuing 
 sudo chown -R pocket /mnt/data
+
+# Start the Pocket service
+systemctl daemon-reload
+systemctl enable pocket.service
+systemctl start pocket.service
