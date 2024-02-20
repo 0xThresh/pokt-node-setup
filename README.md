2effe4704ce5ee065f3e7007e35ac8aa889f37fa
0xThresh.eth <0xthresh@protonmail.com>
2024-02-19 20:54:42 -0800

    Successfully tested script on a node

--- README.md
+++ README.md
@@ -1,15 +1,11 @@
 # POKT Node Setup Script
-> WARNING: The script in this repository has been very lightly tested, and is not yet appropriate for production node setup. Please make sure you understand what you are doing before starting to use this script. 
+WARNING: The script in this repository has been very lightly tested, and is not yet appropriate for production node setup. Please make sure you understand what you are doing before starting to use this script. 
 
-I found that the existing [OPERATE A NODE](https://docs.pokt.network/access-tutorials) documentation seemed outdated and had a few gaps. I documented the steps I had to follow instead below, and created a startup script that's working up until the [Create a Pocket wallet account](https://docs.pokt.network/access-tutorials/manual-setup-tutorial/part-3-pocket-configuration#create-a-pocket-wallet-account) section. 
+I found that the existing [OPERATE A NODE](https://docs.pokt.network/access-tutorials) documentation seemed outdated and had a few gaps. I documented the steps I had to follow instead below, and created a startup script that includes all of the steps to run on a node. 
 
 ## Node Setup
-When I tried to sign up for Linode, I was immediately told I had to give more info about myself before I could use the node type recommended in the Pocket docs. As a result, I went back to AWS and chose to spin up an `m6a.large` instance there instead. 
-
-The docs also reference that you need 800GB of storage; however, the current snapshot file is over 950GB, which means you're going to need at least 1TB of storage on your instance. 
-
-## Snapshot files
-The snapshot site referenced in the documentation no longer exists. I was able to find instructions on where to find a snapshot file here: https://github.com/pokt-network/pocket-core/blob/staging/doc/guides/snapshot.md
+When I tried to sign up for Linode, I was immediately told I had to give more info about myself before I could use the node type recommended in the Pocket docs. As a result, I went back to AWS and chose to spin up an `m6a.2xlarge` instance there instead. 
 
 ## Running the Script
-I opted to run the `node-setup.sh` script as EC2 user data on AWS so that I didn't need to sign onto the host to get the process started. However, you can just as well copy the script into the EC2 instance via SSH if you'd prefer to watch the steps run through. Note that copying the snapshot files takes hours. 
+Write the script to your Ubuntu instance. The script takes a single argument: the name of the DNS zone you plan to deploy a new record into. Usage can be seen with an example domain below:
+`sudo ./node-setup.sh example.com`
--- node-setup.sh
+++ node-setup.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
 
-# Variables:
-# POKT_VERSION - the version of pocket-core to pull
-
 # User setup
 useradd -m -g sudo -s /bin/bash pocket
 
 # Set required vars
 POKT_VERSION="RC-0.11.1" 
+DNS_HOSTNAME=$1
+read -sp "Enter the password to be used for the Pocket account: " POCKET_ACCOUNT_PASSWORD
 
 # When using AWS, skip volume mount 
-sudo mkdir /mnt/data
+mkdir /mnt/data
 
 # Move the home directory to the location of the data directory
-sudo usermod -d /mnt/data pocket 
+usermod -d /mnt/data pocket 
 
 # Update system packages
-sudo apt update
-sudo apt dist-upgrade -y
+apt update
+apt dist-upgrade -y
 
 # Download required packages
-sudo apt-get install -y git build-essential curl file nginx certbot python3-certbot-nginx jq aria2
+apt-get install -y git build-essential curl file nginx certbot python3-certbot-nginx jq aria2
 
 # Download Go
 cd /mnt/data 
 wget https://dl.google.com/go/go1.19.2.linux-amd64.tar.gz
-sudo tar -xvf go1.19.2.linux-amd64.tar.gz
-echo 'export PATH=$PATH:$HOME/go/bin' >> /mnt/data/.profile
-echo 'export GOPATH=$HOME/go' >> /mnt/data/.profile
-echo 'export GOBIN=$HOME/go/bin' >> /mnt/data/.profile
-source /mnt/data/.profile 
+tar -xf go1.19.2.linux-amd64.tar.gz
+echo 'export PATH=$PATH:/mnt/data/go/bin' >> /mnt/data/.bashrc
+echo 'export GOPATH=/mnt/data/go' >> /mnt/data/.bashrc
+echo 'export GOBIN=/mnt/data/go/bin' >> /mnt/data/.bashrc
+source /mnt/data/.bashrc
+export HOME=/mnt/data
 
 # Download Pocket 
-sudo mkdir -p $GOPATH/src/github.com/pokt-network
-cd $GOPATH/src/github.com/pokt-network
-sudo git clone https://github.com/pokt-network/pocket-core.git
+mkdir -p /mnt/data/go/src/github.com/pokt-network
+cd /mnt/data/go/src/github.com/pokt-network
+git clone https://github.com/pokt-network/pocket-core.git
 cd pocket-core
-sudo git checkout tags/$POKT_VERSION
-go build -o $GOPATH/bin/pocket $GOPATH/src/github.com/pokt-network/pocket-core/app/cmd/pocket_core/main.go
+git checkout tags/$POKT_VERSION
+go build -o /mnt/data/go/bin/pocket /mnt/data/go/src/github.com/pokt-network/pocket-core/app/cmd/pocket_core/main.go
 
 # TODO: Only continue if var below isn't null to ensure setup worked correctly 
 POCKET_CLI_VERSION=$(pocket version)
@@ -49,30 +49,30 @@ mkdir -p .pocket/data
 cd .pocket/data 
 
 # Download snapshot file in the background
-wget -O latest.txt https://pocket-snapshot.liquify.com/files/latest.txt
-latestFile=$(cat latest.txt)
-wget -c "https://pocket-snapshot.liquify.com/files/$latestFile" -O - | tar -xv -C /mnt/data/.pocket/data
-rm latest.txt
+# TODO: Choose the file to download based on node type passed into script 
+#wget -c "https://pocket-snapshot.liquify.com/files/pruned/(curl -s https://pocket-snapshot.liquify.com/files/pruned/latest.txt)" -O - | tar -xv -C /mnt/data/.pocket
 
-# TODO: Enter account creation steps 
-
-# Take a backup of the existing config file
-cp /mnt/data/.pocket/config/config.json /mnt/data/.pocket/config/config.json.bak 
+# Create Pocket account and set as validator
+echo "$POCKET_ACCOUNT_PASSWORD" | pocket accounts create
+ACCOUNTS=$(pocket accounts list)
+ADDRESS=$(echo "$ACCOUNTS" | grep -oE '\([0-9]+\)[[:space:]]+[a-fA-F0-9]+' | cut -d' ' -f2)
+echo "$POCKET_ACCOUNT_PASSWORD" | pocket accounts set-validator $ADDRESS
 
 # Insert seeds into config
 export SEEDS=$(curl -s https://raw.githubusercontent.com/pokt-network/pocket-seeds/main/mainnet.txt \
 | tr '\n' ',' \
 | sed 's/,*$//')
+
 pocket util print-configs \
 | jq --arg seeds "$SEEDS" '.tendermint_config.P2P.Seeds = $seeds' \
 | jq '.pocket_config.rpc_timeout = 15000' \
 | jq '.pocket_config.rpc_port = "8082"' \
 | jq '.pocket_config.remote_cli_url = "http://localhost:8082"' \
-| jq . > ~/.pocket/config/config.json
+| jq . > /mnt/data/.pocket/config/config.json
 
 # Create chains.json using default settings
 # TODO: Use 'pocket util', not doing that for now since it requires user input 
-cat << EOF > /mnt/data/.pocket/config/chains.json
+cat <<EOF > /mnt/data/.pocket/config/chains.json
 [
   {
     "id": "0001",
@@ -87,12 +87,12 @@ EOF
 
 # Create genesis.json
 cd /mnt/data/.pocket/config
-wget https://raw.githubusercontent.com/pokt-network/pocket-network-genesis/master/mainnet/genesis.json genesis.json
+wget https://raw.githubusercontent.com/pokt-network/pocket-network-genesis/master/mainnet/genesis.json 
 
 # Increase pocket user's ulimit 
 echo "pocket soft nofile 16384" >> /etc/security/limits.conf
 
-cat << EOF > /etc/systemd/system/pocket.service
+cat <<EOF > /etc/systemd/system/pocket.service
 [Unit]
 Description=Pocket Service
 After=network.target mnt-data.mount
@@ -109,9 +109,87 @@ WantedBy=default.target
 EOF
 
 # Make sure all files are owned by the Pocket user before continuing 
-sudo chown -R pocket /mnt/data
+chown -R pocket:sudo /mnt/data
 
 # Start the Pocket service
 systemctl daemon-reload
 systemctl enable pocket.service
 systemctl start pocket.service
+
+# Register the cert with your domain
+certbot --nginx --domain pokt.$DNS_HOSTNAME --register-unsafely-without-email --no-redirect --agree-tos
+
+# Create the required NGINX config 
+cat <<EOF > /etc/nginx/sites-available/pocket 
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+
+    index index.html index.htm index.nginx-debian.html;
+
+    server_name _;
+
+    location / {
+        try_files $uri $uri/ =404;
+    }
+}
+
+server {
+    add_header Access-Control-Allow-Origin "*";
+    listen 80 ;
+    listen [::]:80 ;
+    listen 8081 ssl;
+    listen [::]:8081 ssl;
+
+    root /var/www/html;
+
+    index index.html index.htm index.nginx-debian.html;
+
+    server_name pokt.$DNS_HOSTNAME;
+
+    location / {
+        try_files $uri $uri/ =404;
+    }
+
+    listen [::]:443 ssl ipv6only=on;
+    listen 443 ssl;
+
+    ssl_certificate /etc/letsencrypt/live/pokt.$DNS_HOSTNAME/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/pokt.$DNS_HOSTNAME/privkey.pem;
+
+    include /etc/letsencrypt/options-ssl-nginx.conf;
+    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
+
+    access_log /var/log/nginx/reverse-access.log;
+    error_log /var/log/nginx/reverse-error.log;
+
+    location ~* ^/v1/client/(dispatch|relay|challenge|sim) {
+        proxy_pass http://127.0.0.1:8082;
+        add_header Access-Control-Allow-Methods "POST, OPTIONS";
+        allow all;
+    }
+
+    location = /v1 {
+        add_header Access-Control-Allow-Methods "GET";
+        proxy_pass http://127.0.0.1:8082;
+        allow all;
+    }
+}
+EOF
+
+systemctl stop nginx
+chown -R www-data /etc/letsencrypt/live/pokt.$DNS_HOSTNAME
+rm /etc/nginx/sites-enabled/default
+ln -s /etc/nginx/sites-available/pocket /etc/nginx/sites-enabled/pocket
+systemctl start nginx
+
+# Enable ufw rules
+ufw enable
+ufw default deny
+ufw allow ssh
+ufw allow 80
+ufw allow 443
+ufw allow 8081
+ufw allow 26656
